13e85e8f68108595406e002c11831e4aa3f63048
DESKTOP-5RDL18S\mxr78 <ntd.64.trade@gmail.com>
2021-09-25 15:09:53 +0900

    no message

--- NewsAPI-getEVR20210925.py
+++ NewsAPI-getEVR20210925.py
@@ -5,11 +5,26 @@ import json
 newsapi = NewsApiClient(api_key='2a644345d7834938ba86a705c41c378c')
 
 # qと国で記事全体を取得
-AllArticles = newsapi.get_everything(q='crypt', country='us')
+response = newsapi.get_everything(q='bitcoin', language='en')
+#headlines = newsapi.get_top_headlines(category='business', country='us')
 
-if (AllArticles['totalResults'] > 0):
-    print(AllArticles['articles'][0:9])
+#この間でJSON データをスライスしたい。
+
+for response_key in response:
+    print(response_key)
+
+'''def extract_data(response):
+    for key in response['response'].keys():
+        if not key.isdigit():
+            continue
+        d = response[articles][description][publishedAt]
+        print(d)
+'''
+
+# print AllArticles
+'''if (response['totalResults']>0):
+    print(response['articles'][0:9])
 else:
-    print('No News are found')
+    print('No News are found')'''
 
 # 取得したトップニュースの１件を表示 print(headlines['articles'][0])
